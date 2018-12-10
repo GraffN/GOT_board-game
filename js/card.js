e4d731f278a4ffc4937f7ea11d5b5ed9f9913171
cameronmoodley <moodley.cameron@gmail.com>
2018-12-10 14:55:04 +0200

    Skeleton Code created

--- js/card.js
+++ js/card.js
@@ -46,7 +46,8 @@ Promise.all(promises).then(character => {
     
     for (i = 0; i < cardNumb; i++) {
     
-        container.innerHTML += "<div class='card' data-attribute="+(i+1)+"><div class='card--img'><img src="+images[i]+" alt='character picture'></div><div class='card--info'><p class='card--info__name'> "+character[i].name+" </p><p class='card--info__gender'>"+character[i].gender+"</p><p class='card--info__age'></p><p class='card--info__title'>Titles: "+character[i].titles+"</p><p class='card--info__alias'>Aliases:"+character[i].aliases+"<p class='card--info__born'>Born:"+character[i].born+"<p class='card--info__died'>Died:"+character[i].died+"<p class='card--info__playedBy'>played by "+character[i].playedBy+"</p><div class='card--sigil'><img id='sigil' src="+sigils[i]+" alt='character sigil'></div><button type='button' id='btnSelect'>Select</button> </div>"
+        container.innerHTML += "<div class='card' data-attribute="+(i+1)+"><div class='card--img'><img src="+images[i]+" alt='character picture'></div><div class='card--info'><p class='card--info__name'> "+character[i].name+" </p><p class='card--info__gender'>"+character[i].gender+"</p><p class='card--info__age'></p><p class='card--info__title'>Titles: "+character[i].titles+"</p><p class='card--info__alias'>Aliases:"+character[i].aliases+"<p class='card--info__born'>Born:"+character[i].born+"<p class='card--info__died'>Died:"+character[i].died+"<p class='card--info__playedBy'>played by "+character[i].playedBy+"</p><div class='card--sigil'><img id='sigil' src="+sigils[i]+" alt='character sigil'></div>"+
+        +"<button type='button'onclick=addPlayerToLocalStorage('"+ urlNeedsToGoHere +"')>Select</button></div>"
         
         /*container.innerHTML += "<div class='card' data-attribute="+(i+1)+"><div class='card--header'><h1 class='card--header__text'>This is a test Card for Noroff, it is number "+(i+1)+"</h1></div><div class='card--name'><p>By "+names[i]+"</div><div class='card--timer'>"+date+"</div><button class='card--button'>View on site</button></div></div>"*/;
     }
@@ -55,13 +56,18 @@ Promise.all(promises).then(character => {
    
 });
 
-var btnSelect = document.getElementById("btnSelect");
-var sigil = document.getElementById("sigil");
-
-
-btnSelect.onclick = function(){
-    var sigilNumb = sigil.scr;
-    console.log(sigilNumb);
+function addPlayerToLocalStorage(URL){
+    count = 0;
+    if(count < 2){
+        count++
+        if(count === 1 ){
+            localStorage.setItem('Player1', URL);
+        }
+        else{
+            localStorage.setItem('Player2', URL);
+            // create a redirect to the boardgame page here
+        }
+    }
 }
 
 
